2770284f9f965d5aa56d388300c4e9389ce2a1ac
EnriqueTor <etorrendell@gmail.com>
2017-01-08 15:21:29 -0500

    add prepare for segue, logics and functionality

--- Rock, Paper Scissor/ViewController.swift
+++ Rock, Paper Scissor/ViewController.swift
@@ -16,10 +16,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var paperButton: UIButton!
     @IBOutlet weak var scissorButton: UIButton!
     
+    // MARK: - Variables
     
-    
-    
-    
+    var playerPlay: String = ""
+    var computerPlay: String = ""
+    let optionsPlay: [String] = ["rock", "paper", "scissor"]
+    var result = ""
     
     
     
@@ -28,32 +30,106 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
     
     // MARK: - Actions
     
-    @IBAction func rockPushed(_ sender: Any) {
+    @IBAction func rockPushed(_ sender: UIButton) {
+        
+        playerHand(sender: sender)
+        computerHand()
+        letsPlay()
+
+        print(playerPlay)
+        print(computerPlay)
+        print(result)
         
         performSegue(withIdentifier: "resultSegue", sender: self)
         
     }
     
-    @IBAction func paperPushed(_ sender: Any) {
+    @IBAction func paperPushed(_ sender: UIButton) {
         
+        playerHand(sender: sender)
+        computerHand()
+        letsPlay()
+        
+        print(playerPlay)
+        print(computerPlay)
+        print(result)
         
         performSegue(withIdentifier: "resultSegue", sender: self)
     }
     
-    @IBAction func scissorPushed(_ sender: Any) {
+    @IBAction func scissorPushed(_ sender: UIButton) {
+        
+        playerHand(sender: sender)
+        computerHand()
+        letsPlay()
+        
+        print(playerPlay)
+        print(computerPlay)
+        print(result)
         
         performSegue(withIdentifier: "resultSegue", sender: self)
     }
     
     
+    func letsPlay() {
+        
+        results()
+        
+    }
     
+    func playerHand(sender: UIButton) {
+        
+        playerPlay = (sender.currentImage?.accessibilityIdentifier)!
+        
+    }
     
+    func computerHand() {
+        
+        let random = Int(arc4random_uniform(UInt32(optionsPlay.count)))
+        
+        computerPlay = optionsPlay[random]
+        
+    }
     
+    func results() {
+        
+        // TIE
+        if playerPlay == "rock" && computerPlay == "rock" || playerPlay == "paper" && computerPlay == "paper" || playerPlay == "scissor" && computerPlay == "scissor" {
+            
+            result = "TIE"
+        }
+        
+        // PLAYER WON
+        if playerPlay == "paper" && computerPlay == "rock" || playerPlay == "rock" && computerPlay == "scissor" || playerPlay == "scissor" && computerPlay == "paper" {
+            
+            result = "PLAYER WON"
+        }
+        
+        // COMPUTER WON
+        if playerPlay == "paper" && computerPlay == "scissor" || playerPlay == "rock" && computerPlay == "paper" || playerPlay == "scissor" && computerPlay == "rock" {
+            
+            result = "COMPUTER WON"
+        }
+    }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "resultSegue" {
+            
+            var dest = segue.destination as! ResultViewController
+            
+            dest.whoWon.text = result
+            
+        }
+        
+        
+    }
 }
 
